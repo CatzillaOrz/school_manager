75617c1d56a6a4e60566d96baad3b342c8a634a9
王俊 <wangjun@dinglicom.com>
2017-05-11 18:37:40 +0800

    'setschool'

--- client/services/common.js
+++ client/services/common.js
@@ -5,7 +5,7 @@ angular.module('dleduWebService')
         return {
             product: {
                 name: '知新网综合平台',
-                version: '0.0.1.5'
+                version: '0.0.1.7'
 
             },
             isMSIE789: function () {
