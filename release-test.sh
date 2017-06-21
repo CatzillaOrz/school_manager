fcfb16d92049fa45cdd8f9c4c5989763d20cb965
王俊 <wangjun@dinglicom.com>
2017-06-21 17:36:04 +0800

    'commit'

--- release-test.sh
+++ release-test.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-########################## For prod env ######################################
+########################## For test env ######################################
 
 export OPENSHIFT_NODEJS_PORT=9009
 export NODE_ENV=production
-export BACKEND_API=http://dledu.dlztc.com/zhixin_api
-export DIANDIAN_API=http://dd.dlztc.com/diandian_api
-export EM_API=http://em.dlztc.com/em_api2
-export HY_API=http://hy.dlztc.com/ew_api
-export IO_API=http://io.dlztc.com
-export API_GATEWAY=http://apigateway.dlztc.com/
+export BACKEND_API=http://dledutest.aizhixin.com/zhixin_api
+export DIANDIAN_API=http://ddtest.aizhixin.com/diandian_api
+export EM_API=http://em2.aizhixintest.com/em_api2
+export HY_API=http://hy.aizhixintest.com/ew_api
+export IO_API=http://iotest.aizhixin.com
+export API_GATEWAY=http://172.16.23.122:3333/
 
-#redis共享前缀
-export SESSION_REDIS_HOST=172.16.23.31
+export SESSION_REDIS_HOST=172.16.23.32
 export SESSION_REDIS_PORT=6379
+export SESSION_REDIS_DB='0'
 export SESSION_REDIS_PASS=
-export SESSION_REDIS_DB=0
 export SESSION_REDIS_PREFIX=dledu_web_session
-export SESSION_DOMAIN=dlztc.com
+export SESSION_DOMAIN=aizhixin.com
 pm2 start server/app.js -i 1 --name 'DLEDU_school_manager'
