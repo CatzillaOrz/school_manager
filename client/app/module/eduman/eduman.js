dce325c0308851d92f9911b8a70889b72576881e
xingang.yang <xingang.yang@dinglicom.com>
2017-06-23 14:23:20 +0800

    '评教问卷统计和分配页面'

--- client/app/module/eduman/eduman.js
+++ client/app/module/eduman/eduman.js
@@ -94,6 +94,48 @@ angular.module('dleduWebApp')
 					label: '评教问卷分配页面'
 				}
 			})
+			.state('evaquestatic', {
+				parent: 'evaquestion',
+				url   : '/evaquestatic/:id',
+				access: {requiredLogin: true},
+				views : {
+					'content@base': {
+						controller : 'EvaQueStaticCtrl',
+						templateUrl: 'app/module/eduman/evaquestatic.html'
+					}
+				},
+				ncyBreadcrumb: {
+					label: '评教统计'
+				}
+			})
+			.state('evaquesadd', {
+				parent: 'evaquestion',
+				url   : '/evaquesadd',
+				access: {requiredLogin: true},
+				views : {
+					'content@base': {
+						controller : 'EvaQueStaticCtrl',
+						templateUrl: 'app/module/eduman/evaquesmod.html'
+					}
+				},
+				ncyBreadcrumb: {
+					label: '新增评教问卷'
+				}
+			})
+			.state('evaquesedit', {
+				parent: 'evaquestion',
+				url   : '/evaquesedit/:id',
+				access: {requiredLogin: true},
+				views : {
+					'content@base': {
+						controller : 'EvaQueStaticCtrl',
+						templateUrl: 'app/module/eduman/evaquesmod.html'
+					}
+				},
+				ncyBreadcrumb: {
+					label: '编辑评教问卷'
+				}
+			})
 			.state('elecfence', {
 				parent: 'base',
 				url   : '/elecfence',
