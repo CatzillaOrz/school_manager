2a38601c18cae62a3475ac9e28a1062508c1dd1b
毋闻鹏 <wenpeng.wu@dinglicom.com>
2017-06-30 15:49:29 +0800

    1

--- release-dev.sh
+++ release-dev.sh
@@ -3,7 +3,6 @@
 export OPENSHIFT_NODEJS_PORT=9009
 export NODE_ENV=production
 export BACKEND_API=http://dledudev.aizhixin.com/zhixin_api
-export DIANDIAN_API=http://dddev.aizhixin.com/diandian_api
 export EM_API=http://emdev.aizhixin.com/em_api2
 export HY_API=http://hydev.aizhixin.com/ew_api
 export IO_API=http://iodev.aizhixin.com
@@ -18,4 +17,3 @@ export SESSION_REDIS_PREFIX=dledu_web_session
 export SESSION_DOMAIN=aizhixin.com
 
 pm2 start server/app.js -i 1 --name 'DLEDU_school_manager'
-
