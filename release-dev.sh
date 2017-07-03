4f0cb8fa8b9e9efeb5da771a258f167976578291
王俊 <wangjun@dinglicom.com>
2017-07-03 11:28:17 +0800

    change dev release shell

--- release-dev.sh
+++ release-dev.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
 ########################## For develop env ######################################
 export OPENSHIFT_NODEJS_PORT=9009
 export NODE_ENV=production
 export BACKEND_API=http://dledudev.aizhixin.com/zhixin_api
+export DIANDIAN_API=http://dddev.aizhixin.com/diandian_api
 export EM_API=http://emdev.aizhixin.com/em_api2
 export HY_API=http://hydev.aizhixin.com/ew_api
 export IO_API=http://iodev.aizhixin.com
