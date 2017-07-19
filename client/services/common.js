221a50fa4adeb3bea7aa59556ecd527dc9709c77
王俊 <wangjun@dinglicom.com>
2017-07-19 16:38:52 +0800

    'fix'

--- client/services/common.js
+++ client/services/common.js
@@ -6,7 +6,7 @@ angular.module('dleduWebService')
             product: {
                 name: '知新网综合平台',
 
-                version: '0.0.3.6'
+                version: '0.0.3.7'
 
             },
             isMSIE789: function () {
