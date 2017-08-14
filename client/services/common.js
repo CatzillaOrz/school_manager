a6257a759cf431d5d5c1fa48ad8d26f0bdb6f7db
xingang.yang <xingang.yang@dinglicom.com>
2017-08-14 14:48:23 +0800

    '修改企业管理bug'

--- client/services/common.js
+++ client/services/common.js
@@ -6,7 +6,7 @@ angular.module('dleduWebService')
             product: {
                 name: '知新网综合平台',
 
-                version: '0.0.4.6'
+                version: '0.0.4.7'
 
             },
             isMSIE789: function () {
