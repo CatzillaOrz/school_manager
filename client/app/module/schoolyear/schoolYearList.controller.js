186f47e5b198b77e211365239edce2df0f2960a3
王俊 <wangjun@dinglicom.com>
2017-10-10 15:05:38 +0800

    'fixschoolyeartab'

--- client/app/module/schoolyear/schoolYearList.controller.js
+++ client/app/module/schoolyear/schoolYearList.controller.js
@@ -285,5 +285,57 @@ angular.module('dleduWebApp')
 
         };
         $scope.preiodFn.init();
+        var TabBlock = {
+            s: {
+                animLen: 200
+            },
 
-    })
+            init: function() {
+                TabBlock.bindUIActions();
+                TabBlock.hideInactive();
+            },
+
+            bindUIActions: function() {
+                $('.tabBlock-tabs').on('click', '.tabBlock-tab', function(){
+                    TabBlock.switchTab($(this));
+                });
+            },
+
+            hideInactive: function() {
+                var $tabBlocks = $('.tabBlock');
+
+                $tabBlocks.each(function(i) {
+                    var
+                        $tabBlock = $($tabBlocks[i]),
+                        $panes = $tabBlock.find('.tab-pane'),
+                        $activeTab = $tabBlock.find('.tabBlock-tab.active');
+
+                    $panes.hide();
+                    $($panes[$activeTab.index()]).show();
+                });
+            },
+
+            switchTab: function($tab) {
+                var $context = $tab.closest('.tabBlock');
+
+                if (!$tab.hasClass('active')) {
+                    $tab.siblings().removeClass('active');
+                    $tab.addClass('active');
+
+                    TabBlock.showPane($tab.index(), $context);
+                }
+            },
+
+            showPane: function(i, $context) {
+                var $panes = $context.find('.tab-pane');
+
+                // Normally I'd frown at using jQuery over CSS animations, but we can't transition between unspecified variable heights, right? If you know a better way, I'd love a read it in the comments or on Twitter @johndjameson
+                $panes.slideUp(TabBlock.s.animLen);
+                $($panes[i]).slideDown(TabBlock.s.animLen);
+            }
+        };
+
+        $(function() {
+            TabBlock.init();
+        });
+    });
