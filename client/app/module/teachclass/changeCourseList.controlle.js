b5b80178c3e9fe08da966de6ef3e3e020cf601d6
王俊 <wangjun@dinglicom.com>
2017-10-24 17:34:16 +0800

    '时间选择提示处理'

--- client/app/module/teachclass/changeCourseList.controlle.js
+++ client/app/module/teachclass/changeCourseList.controlle.js
@@ -20,9 +20,26 @@ angular.module('dleduWebApp')
                 pageNumber: 0,
                 pageSize: 10
             },
+            checkDateZone:function (startDate,endDate) {
+                var star=new Date(startDate).getTime();
+                var end=new Date(endDate).getTime();
+                if(star<=end){
+                    return true;
+                }else{
+                    return false;
+                }
+
+            },
             getTeacherChangeCourseList:function () {
                 var _this=this;
                 var params=_this.params;
+                if(params.startDate&&params.endDate){
+                    var flag=_this.checkDateZone(params.startDate,params.endDate)
+                    if(!flag){
+                        messageService.openMsg("开始时间小于结束时间！");
+                        return ;
+                    }
+                }
                 params.pageNumber=_this.page.pageNumber;
                 params.pageSize=_this.page.pageSize;
                 TeachClassService.getTeacherChangeCourseList(params).$promise
