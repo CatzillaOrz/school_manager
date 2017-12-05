0b9caa43cf75541be7742dc8b8ece58664e52ceb
catzillaorz <catzillaorz@outlook.com>
2017-12-05 14:30:31 +0800

    查看课表

--- client/app/module/teacher/teacher.js
+++ client/app/module/teacher/teacher.js
@@ -43,6 +43,20 @@ angular.module('dleduWebApp')
                     label: '教师管理'
                 }
             })
+            .state('teacherListSimplify', {
+                parent: 'base',
+                url   : '/teacherListSimplify',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeacherListCtrl',
+                        templateUrl: 'app/module/teacher/teacherListSimplify.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '查看课表'
+                }
+            })
             .state('teacherEdit', {
                 parent: 'teacher',
                 url   : '/teacheredit/:id',
