44aa587c274a8631bd1322da478b57a152962654
xingang.yang <xingang.yang@dinglicom.com>
2017-12-14 11:37:18 +0800

    '修改学期修改bug'

--- client/app/module/student/studentList.controller.js
+++ client/app/module/student/studentList.controller.js
@@ -60,7 +60,7 @@ angular.module('dleduWebApp')
                         id: -1, // the value of the option
                         text: '全部'
                     },
-                    allowClear: true
+                    allowClear: false
                 }
             },
             //专业下拉列表配置
@@ -71,7 +71,7 @@ angular.module('dleduWebApp')
                         id: -1, // the value of the option
                         text: '全部'
                     },
-                    allowClear: true,
+                    allowClear: false,
                     ajax: {
                         url: "api/major/getMajorDropList",
                         dataType: 'json',
@@ -116,7 +116,7 @@ angular.module('dleduWebApp')
                         id: -1, // the value of the option
                         text: '全部'
                     },
-                    allowClear: true,
+                    allowClear: false,
                     ajax: {
                         url: "api/class/geClassDropList",
                         dataType: 'json',
