c6435bc28398aa144c90278a7e1fa90eaff11221
王俊 <wangjun@dinglicom.com>
2018-02-05 17:06:29 +0800

    '修改路由'

--- client/app/app.js
+++ client/app/app.js
@@ -133,9 +133,7 @@ angular.module('dleduWebApp', [
             //入口路由配置
             $urlRouterProvider
                 .otherwise('/apprenticeship');
-        }else if(domain=='gllg'||domain=='glut'){
-            $urlRouterProvider
-                .otherwise('/templet1');
+       git
         }else{
 
             //入口路由配置
