61624b40dd3d01d0aa919d078df9776dfdfb87b6
xingang.yang <xingang.yang@dinglicom.com>
2018-04-12 14:49:10 +0800

    '修改评价问卷为学生评教功能'

--- client/app/module/eduman/evaquesmod.controller.js
+++ client/app/module/eduman/evaquesmod.controller.js
@@ -3,11 +3,20 @@
  * 评教问卷新增，修改页面
  */
 angular.module('dleduWebApp')
-	.controller('EvaQuesModCtrl', function ($scope, $state, $timeout, AuthService, EduManService, messageService, $filter) {
+	.controller('EvaQuesModCtrl', function ($scope, $state, $timeout, AuthService, EduManService, messageService,
+	                                        $filter, ngDialog) {
 		$scope.evaQuesModFn={
 			isEditOrAdd: false, //true是编辑 false是新增
 			//问卷信息
 			record: null,
+
+			page: {
+				totalElements: 0,
+				totalPages: 0,
+				pageNumber: 1,
+				pageSize: 10
+			},
+
 			id: $state.params.id,
 			params: {
 				name: '',
@@ -23,18 +32,30 @@ angular.module('dleduWebApp')
 			quesLists: [],//临时保存题目
 
 			// 获取评教问卷信息
-			getEvaQuesInfo: function () {
+			getEvaQuesInfo: function (id, type) {
 				var that = this;
 				var params = {
 					orgId: AuthService.getUser().orgId,
-					id: that.id
+					id: id
 				};
 				EduManService.getEvaQuesInfo(params).$promise
 					.then(function (data) {
-						that.params = data;
-						that.params.endDate = that.params.endDate.substring(0, 10);
-						that.convertRadio(that.params);
-						that.quesLists = that.params.questions;
+						if(!that.isEditOrAdd){ //新增
+							that.record = data;
+							that.record.name = '';
+							that.record.endDate = '';
+						}else{
+							that.params = data;
+							that.params.endDate = that.params.endDate.substring(0, 10);
+							that.convertRadio(that.params);
+							that.quesLists = that.params.questions;
+						}
+
+						if(type){
+							that.params = that.record;
+							that.convertRadio(that.params);
+							that.quesLists = that.params.questions;
+						}
 					})
 					.catch(function (error) {
 
@@ -58,6 +79,7 @@ angular.module('dleduWebApp')
 				var temp = angular.copy(this.quesLists);
 				this.quesLists = [];
 				this.quesLists = temp;
+				this.calcAllTotal(this.quesLists);
 			},
 
 			//增加题目选项
@@ -99,14 +121,6 @@ angular.module('dleduWebApp')
 					}
 				}
 
-				//转换题目类型保持和之前类型一致
-				if(this.params.choiceType == 'score'){
-					this.params.quantification = true;
-					this.params.choiceQuestion = false;
-				}else if(this.params.choiceType == 'choice'){
-					this.params.choiceQuestion = true;
-					this.params.quantification = false;
-				}
 				if($state.params.id){
 					var cloneParams = angular.copy(params);
 					cloneParams.endDate = cloneParams.endDate + ' 23:59:59';
@@ -147,18 +161,34 @@ angular.module('dleduWebApp')
 			 */
 			convertRadio: function(params){
 				//转换题目类型
-				if(this.params.quantification == true){
+				if(params.quantification == true){
 					this.params.choiceType = 'score';
-				}else if(this.params.choiceQuestion = true){
-					this.params.choiceType == 'choice'
+				}else if(params.choiceQuestion == true){
+					this.params.choiceType = 'choice'
 				}
 				var questions = params.questions;
+				if(!this.isEditOrAdd){
+					delete params.id;
+					delete params.createDate;
+					delete params.status;
+					delete params.isEnd;
+					delete params.teachingNum;
+				}
 				for(var i = 0, len = questions.length; i < len; i++){
 					var temp = questions[i];
 					if(temp.radio){
 						temp.radio = false;
 					}else{
 						temp.radio = true;
+					}
+					//清空选项id值
+					if(!this.isEditOrAdd){
+						temp.id = 0;
+						var choice = temp.questionChioce;
+						for(var j = 0, choiceLen = choice.length; j < choiceLen; j++){
+							choice[j].id = 0;
+							delete  choice[j].questionId;
+						}
 					}
 				}
 			},
@@ -248,8 +278,67 @@ angular.module('dleduWebApp')
 			 * 监控量化选择选项变化
 			 */
 			monitorOptionChange: function(){
+				//转换题目类型保持和之前类型一致
+				if(this.params.choiceType == 'score'){
+					this.params.quantification = true;
+					this.params.choiceQuestion = false;
+				}else if(this.params.choiceType == 'choice'){
+					this.params.choiceQuestion = true;
+					this.params.quantification = false;
+				}
 				this.emptyDataScore(this.quesLists);//清空选择分数
 				this.params.totalScore = 0; //清空总分
+			},
+
+			// 获取评教问卷列表
+			getEvaQuesList: function () {
+				var that = this;
+				var params = {
+					orgId: AuthService.getUser().orgId,
+					pageNumber: that.page.pageNumber,
+					pageSize: that.page.pageSize
+				};
+				EduManService.getEvaQuesList(params).$promise
+					.then(function (data) {
+						that.records = data.data;
+						that.page = data.page;
+					})
+					.catch(function (error) {
+
+					})
+			},
+
+
+			//导入问卷内容
+			importTemplate: function(){
+				this.page.pageNumber = 1;
+				if(!this.records){
+					this.getEvaQuesList();
+				}
+				ngDialog.open({
+					template: 'quelistDialog',
+					width: 700,
+					scope: $scope
+				})
+			},
+
+			/**
+			 * 预览问卷
+			 */
+			preViewQue: function($index){
+				var id = this.records[$index].id;
+				this.getEvaQuesInfo(id);
+				ngDialog.open({
+					template: 'queInfoDialog',
+					width: 700,
+					scope: $scope
+				})
+			},
+
+			//点击导入数据
+			impData: function($index){
+				var id = this.records[$index].id;
+				this.getEvaQuesInfo(id, 'imp');
 			},
 
 			init: function () {
@@ -257,6 +346,13 @@ angular.module('dleduWebApp')
 					this.isEditOrAdd = true;
 					this.getEvaQuesInfo($state.params.id);
 				}else{
+					if(this.params.choiceType == 'score'){
+						this.params.quantification = true;
+						this.params.choiceQuestion = false;
+					}else if(this.params.choiceType == 'choice'){
+						this.params.choiceQuestion = true;
+						this.params.quantification = false;
+					}
 					this.quesLists.push({
 						name: '',//题目
 						score: 0,//分数
