90febdf9b46aad7f19f8c42b8dfabc0f79ca866b
catzillaorz <catzillaorz@outlook.com>
2018-04-26 09:13:04 +0800

    实训-教学统计

--- client/app/module/statistics/statistics.js
+++ client/app/module/statistics/statistics.js
@@ -20,4 +20,60 @@ angular.module('dleduWebApp')
                     label: '实践教学汇总'
                 }
             })
+            .state('studentAttending', {
+                parent: 'base',
+                url   : '/studentAttending',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/studentAttending.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '学生参与明细表'
+                }
+            })
+            .state('studentActive', {
+                parent: 'base',
+                url   : '/studentActive',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/studentActive.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '学生激活明细表'
+                }
+            })
+            .state('stuProcess', {
+                parent: 'base',
+                url   : '/stuProcess',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/stuProcess.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '学生参与过程明细'
+                }
+            })
+            .state('stuJournal', {
+                parent: 'base',
+                url   : '/stuJournal',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/stuJournal.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '学生日志明细表'
+                }
+            })
 	});
--- client/app/module/statistics/teachingSummary.controller.js
+++ client/app/module/statistics/teachingSummary.controller.js
@@ -3,7 +3,7 @@
  * 实践小组管理
  */
 angular.module('dleduWebApp')
-	.controller('TeachingSummaryCtrl', function ($scope, $state, AuthService, EduManService, messageService, CommonService,
+	.controller('TeachingSummaryCtrl', function ($scope, $state, AuthService, EduManService, messageService, CollegeService, CommonService, Select2LoadOptionsService, ClassService,
 		PracticeManService) {
 		$scope.summaryFn = {
 			page:{
@@ -11,7 +11,130 @@ angular.module('dleduWebApp')
 				pageNumber: 2,
 				pageSize: 10,
 			},
-			getPracticeGroupList: function(){}
+			params: {
+                name:"",
+                collegeId:"",
+                professionalId:"",
+                masterName:"",
+                teachingYear:""
+			},
+            majorDropList: [],
+            someArr: ['temp1','temp2','temp3','temp4','temp5','temp6','temp7','temp8','temp9','temp10'],
+			//select2动态关键字查询列表配置
+            selectCollege2Options: function () {
+                var _this = this;
+                return {
+                    placeholder: {
+                        id: -1, // the value of the option
+                        text: '全部'
+                    },
+                    allowClear: true,
+                    ajax: Select2LoadOptionsService.getLoadOptions("api/college/getCollegeDropList", {
+                        orgId: AuthService.getUser().orgId,
+                        pageNumber: 1,
+                        pageSize: 1000,
+                        managerId: AuthService.getUser().id
+                    }, "name"),
+
+                    templateResult: function (data) {
+
+                        if (data.id === '') { // adjust for custom placeholder values
+                            _this.collegeDropList = [];
+                            return '按班级筛选';
+                        }
+                        _this.collegeDropList.push(data);
+                        return data.name;
+                    },
+                }
+            },
+            //专业下拉列表配置
+            select2MajorOptions: function () {
+                var that = this;
+                return {
+                    placeholder: {
+                        id: -1, // the value of the option
+                        text: '全部'
+                    },
+                    allowClear: false,
+                    ajax: {
+                        url: "api/major/getMajorDropList",
+                        dataType: 'json',
+                        //delay: 250,
+                        data: function (query) {
+                            var params = {
+                                orgId: AuthService.getUser().orgId,
+                                pageNumber: 1,
+                                pageSize: 100,
+                                collegeId: that.params.collegeId,
+
+                            };
+                            params.name = query.term;
+                            return params;
+                        },
+                        processResults: function (data, params) {
+                            params.page = params.page || 1;
+                            return {
+                                results: data.data,
+                                pagination: {
+                                    more: (params.page * 30) < data.total_count
+                                }
+                            };
+                        },
+                        cache: false
+                    },
+
+                    templateResult: function (data) {
+                        if (data.id === '') { // adjust for custom placeholder values
+                            that.majorDropList = [];
+                        }
+                        that.majorDropList.push(data);
+                        return data.name;
+                    }
+                }
+            },
+            getCollegeDropList: function () {
+                var that = this;
+                var params = {
+                    orgId: AuthService.getUser().orgId,
+                    pageNumber: 0,
+                    pageSize: 1000,
+                    managerId: AuthService.getUser().id
+                }
+                CollegeService.getCollegeDropList(params).$promise
+                    .then(function (data) {
+                        that.collegeDropList =data.data;
+                    })
+                    .catch(function (error) {
+                    })
+            },
+            // 获取班级列表
+            getClassList: function () {
+                var that = this;
+                var params = {
+                    orgId: AuthService.getUser().orgId,
+                    pageNumber:1,
+                    pageSize: that.page.pageSize,
+                    managerId: AuthService.getUser().id
+                };
+                params.name=that.params.name;
+                params.collegeId=that.params.collegeId;
+                params.professionalId=that.params.professionalId;
+                params.masterName=that.params.masterName;
+                params.teachingYear=that.params.teachingYear;
+                ClassService.getClassList(params).$promise
+                    .then(function (data) {
+                        that.classList = data.data;
+                        that.page=data.page;
+                    })
+                    .catch(function (error) {
+
+                    })
+            },
+			getPracticeGroupList: function(){},
+			init: function(){
+				this.getCollegeDropList();
+			}
 		}
 		console.log('Hello Teacher');
+		$scope.summaryFn.init();
 	});
