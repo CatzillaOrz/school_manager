0d49fd09dfa0e31b760b113e70ae6e7ecc4724f0
catzillaorz <catzillaorz@outlook.com>
2018-04-26 15:22:17 +0800

    实践-统计

--- client/app/module/statistics/statistics.js
+++ client/app/module/statistics/statistics.js
@@ -76,4 +76,88 @@ angular.module('dleduWebApp')
                     label: '学生日志明细表'
                 }
             })
+            .state('impartProcess', {
+                parent: 'base',
+                url   : '/impartProcess',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/impartProcess.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '教师指导过程明细'
+                }
+            })
+            .state('stuRoutineCount', {
+                parent: 'base',
+                url   : '/stuRoutineCount',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/stuRoutineCount.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '签到统计汇总'
+                }
+            })
+            .state('enterpriseDetail', {
+                parent: 'base',
+                url   : '/enterpriseDetail',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/enterpriseDetail.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '实践企业统计表'
+                }
+            })
+            .state('taskDetail', {
+                parent: 'base',
+                url   : '/taskDetail',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/taskDetail.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '实践任务明细'
+                }
+            })
+            .state('stuReport', {
+                parent: 'base',
+                url   : '/stuReport',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/stuReport.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '学生实践报告成绩明细'
+                }
+            })
+            .state('stuScore', {
+                parent: 'base',
+                url   : '/stuScore',
+                access: {requiredLogin: true},
+                views : {
+                    'content@base': {
+                        controller : 'TeachingSummaryCtrl',
+                        templateUrl: 'app/module/statistics/stuScore.html'
+                    }
+                },
+                ncyBreadcrumb: {
+                    label: '学生实习成绩汇总'
+                }
+            })
 	});
