b9a1d21c2504384e9b194926945b33411dfd1027
catzillaorz <catzillaorz@outlook.com>
2018-05-16 16:32:01 +0800

    实践-统计-企业统计 | 报表导出

--- client/services/entity/statistics.js
+++ client/services/entity/statistics.js
@@ -11,6 +11,31 @@ angular.module('dleduWebService')
             getStuJournal: function (params) {
                 var statistics = $resource('api/statistics/getStuJournal');
                 return statistics.save(params);
+            },
+            getEnterpriseDetail: function (params) {
+                var statistics = $resource('api/statistics/getEnterpriseDetail');
+                return statistics.save(params);
+            },
+            exportStuJournal: function(params){
+                return $http({
+					method: 'GET',
+					url: "api/statistics/exportStuJournal",
+					params: params
+				});
+            },
+            exportEnterpriseDetail: function(params){
+                return $http({
+					method: 'GET',
+					url: "api/statistics/exportEnterpriseDetail",
+					params: params
+				});
+            },
+            exportStuProcess: function(params){
+                return $http({
+					method: 'GET',
+					url: "api/statistics/exportStuProcess",
+					params: params
+				});
             }
         }
 
